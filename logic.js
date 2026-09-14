4e18b1ba26893a63cb77c1d3b831bfb9eb742cac
RajpalPawar22 <rajpalpawar777@gmail.com>
2026-09-14 18:44:11 +0530

    Refactor calculator logic for operator handling and update display functionality

--- logic.js
+++ logic.js
@@ -1,32 +1,46 @@
 //THE MEME CALCI
 let currentVal = '';
 let operator='';
-let forDisplay = '';
+let result = 0;
 let ans = document.getElementById("ans");
 let expression = document.getElementById("expression");
-let op;
 
 function addNum (num){
     currentVal+=num;  
-    forDisplay+=num;
+    
     updateDisplay();
 }
 function oprtr(op){
-    operator=op;
-    currentVal+=operator;
+     if (currentVal === '') return; 
+    
+    const lastChar = currentVal.slice(-1);
+    const isOperator = /[+\-*/%]/.test(lastChar);
+    
+    if (isOperator) {
+        currentVal = currentVal.slice(0, -1) + op; 
+       
+    }
+    else {
+        currentVal += op; 
+    }
+   
+    
     updateDisplay();   
 }
 function square(){
     expression.textContent = currentVal +'^2';
-    ans.textContent = currentVal*currentVal;
+    currentVal*=currentVal;
+    ans.textContent =currentVal ;
+
+   
 }
 function updateDisplay(){
-    ans.textContent =currentVal; 
+    ans.textContent = currentVal; 
 }
 
 function show(){
     expression.textContent = currentVal;
-    const result = new Function (`return ${currentVal}`)();
+    result = new Function (`return ${currentVal}`)();
     ans.textContent  = result; 
     currentVal = '';
 }
